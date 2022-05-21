27b9dee6f1bff4e0bd3f2ded1031f5f38221f62b
Stefan Luger <lug.stefan@gmail.com>
2022-05-21 19:36:10 +0200

    Create 04-median-arrays.js

---
+++ 04-median-arrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const arr = [...nums1, ...nums2];
+    arr.sort();
+    
+    if (arr.length % 2 === 0) {
+        return (arr[arr.length/2 - 1] + arr[arr.length/2]) / 2;
+    } else {
+        return arr[Math.floor(arr.length / 2)];
+    }
+};
